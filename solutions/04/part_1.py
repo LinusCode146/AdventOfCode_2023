da371019f97f31fdbd699ab561fac5bc01f9ad6f
Linus Schulze <linus.schulze@denktmit.de>
2023-12-04 16:10:41 +0100

    refactored

--- solutions/04/part_1.py
+++ solutions/04/part_1.py
@@ -1,31 +1,39 @@
 from utils import read_text_file_inline_spaces
 
 
-def find_winning_numbers(file):
+def find_winning_numbers(file_content):
     total_sum = []
     winning = []
     reached_break = False
     current_winning = 0
-    for index, line in enumerate(file):
-        for idx, c in enumerate(line):
-            if c == '|':
+
+    for index, line in enumerate(file_content):
+        for idx, char in enumerate(line):
+            if char == '|':
                 reached_break = True
-            if c.isdigit() and not reached_break:
-                winning.append(int(c))
-            elif c.isdigit() and reached_break:
-                if int(c) in winning:
+            if char.isdigit() and not reached_break:
+                winning.append(int(char))
+            elif char.isdigit() and reached_break:
+                if int(char) in winning:
                     if current_winning == 0:
                         current_winning = 1
                     elif current_winning > 0:
                         current_winning *= 2
-            if idx == len(file[index]) - 1:
+            if idx == len(file_content[index]) - 1:
                 total_sum.append(current_winning)
                 current_winning = 0
                 winning.clear()
                 reached_break = False
+
     return sum(total_sum)
 
 
-if __name__ == '__main__':
+def main():
     doc = read_text_file_inline_spaces()
     solution = find_winning_numbers(doc)
+    return solution
+
+
+if __name__ == '__main__':
+    solution = main()
+
